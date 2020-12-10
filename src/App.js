63cf4c1890fef7f399a85c130fb8346f04b3abe2
thang241190 <thangnguyen24111990@gmail.com>
2020-12-11 01:03:36 +0200

    Add database with json-server and concurrently

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
 import Todo from "./pages/Todo/Todo";
+
+fetch("http://localhost:3050/tasks", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    task: "AAAA",
+    quantity: 1,
+    status: true,
+    edit: false
+  })
+})
+  .then((res) => res.json())
+  .then((tasks) => console.log(tasks));
 const App = () => (
   <Provider store={store}>
     <div className="App">
